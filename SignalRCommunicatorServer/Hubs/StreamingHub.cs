6cfd1282a5f143ee2b1b9e35212b390dc1c3d34f
Peter Dz <piotreq_46@wp.pl>
2024-03-24 10:54:43 +0100

    Add project files.

---
+++ SignalRCommunicatorServer/Hubs/StreamingHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRCommunicatorServer.Hubs
+{
+	public class StreamingHub : Hub
+	{
+		public async Task BroadcastImage(byte[] imageBytes)
+		{
+			await Clients.Others.SendAsync("ReceiveImage", imageBytes);
+		}
+
+		public async Task BroadcastAudio(byte[] audioBytes)
+		{
+			await Clients.Others.SendAsync("ReceiveAudio", audioBytes);
+		}
+	}
+}
